bf65f78f0b70cc1f7f4652a6bef36b93a73f05a3
Enya <Enya@HOME-PC>
2022-08-26 03:22:03 +0300

    done

---
+++ homework_15/Class1.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace homework_15
+{
+    public class SomeException : Exception
+    {
+        public override string Message => "Баланс не может быть отрицательным";
+    }
+}
